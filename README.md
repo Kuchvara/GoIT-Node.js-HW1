00c75410277df328d8a669f8d0ab257ff268a725
Ivan Kuchvara <ivan.kuchvara@gmail.com>
2021-07-06 13:02:29 +0300

    done
    some fixes + upload screenshots

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # GoIT-Node.js-HW1
+
+# Получаем и выводим весь список контактов в виде таблицы (console.table)
+
+https://monosnap.com/file/49exKyWQ25jq6w0HM93xBfjrxOQIp5
+
+# Получаем контакт по id
+
+https://monosnap.com/file/q10hGQcnerf22a8BTmxm4gXQPWeXlq
+
+# Добавялем контакт
+
+https://monosnap.com/file/qZSevFvMsYdRBaq1yxbbfD5J0LIAXg
+
+# Удаляем контакт
+
+https://monosnap.com/file/xTbg1pIDqCSlrBYGVaMNQLF9qCdjs5
--- contacts.js
+++ contacts.js
@@ -16,9 +16,9 @@ async function listContacts() {
 
 async function getContactById(contactId) {
     try {
-        const data = await fs.readFile(contactsPath, 'utf8')
-        const contact = JSON.parse(data).find(el => el.id === contactId)
-        console.log(contact)
+        const data = await fs.readFile(contactsPath)
+        const contact = JSON.parse(data).find(el => el.id === Number(contactId))
+        return console.table(contact)
     }
     catch (err) {
     console.error(err)}
@@ -27,7 +27,7 @@ async function getContactById(contactId) {
 async function removeContact(contactId) {
     try {
         const data = await fs.readFile(contactsPath, 'utf8')
-        const newContacts = JSON.parse(data).filter(el => el.id !== contactId)
+        const newContacts = JSON.parse(data).filter(el => el.id !== Number(contactId))
 
         await fs.writeFile(contactsPath, JSON.stringify(newContacts), 'utf8')
 
